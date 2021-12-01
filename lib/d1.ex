a63e119f8256c614b911c8a0a4d7a285959b8995
Angelo Moreira <angelo.moreira@gmail.com>
2021-12-01 22:45:46 +0000

    day 1 part 2

--- lib/d1.ex
+++ lib/d1.ex
@@ -2,11 +2,16 @@ defmodule D1 do
   @moduledoc false
   def part1(file) do
     file
+    |> conver_to_ints()
+    |> Enum.reduce(%{increased_by: 0, current_depth: nil}, &calculate_depth(&2, &1))
+    |> Map.get(:increased_by)
+  end
+
+  defp conver_to_ints(file) do
+    file
     |> File.read!()
     |> String.split("\n")
     |> Enum.map(&String.to_integer/1)
-    |> Enum.reduce(%{increased_by: 0, current_depth: nil}, &calculate_depth(&2, &1))
-    |> Map.get(:increased_by)
   end
 
   defp calculate_depth(%{current_depth: cur_depth} = acc, depth)
@@ -18,4 +23,38 @@ defmodule D1 do
 
   defp calculate_depth(acc, depth),
     do: Map.put(acc, :current_depth, depth)
+
+  def part2(file) do
+    file
+    |> conver_to_ints()
+    |> calculate_sliding_window(%{increased_by: 0, window: {}})
+  end
+
+  defguard bigger_window(first, second, third, fourth)
+           when second + third + fourth > first + second + third
+
+  defp calculate_sliding_window([new_depth | tail], %{window: {first, second, third}} = acc)
+       when bigger_window(first, second, third, new_depth) do
+    calculate_sliding_window(tail, %{
+      window: {second, third, new_depth},
+      increased_by: Map.get(acc, :increased_by) + 1
+    })
+  end
+
+  defp calculate_sliding_window([new_depth | tail], %{window: {_, second, third}} = acc) do
+    calculate_sliding_window(
+      tail,
+      Map.put(acc, :window, {second, third, new_depth})
+    )
+  end
+
+  # adding tuples to the window until we reach 3
+  defp calculate_sliding_window([head | tail], acc) do
+    new_window = Tuple.append(acc.window, head)
+    acc = Map.put(acc, :window, new_window)
+
+    calculate_sliding_window(tail, acc)
+  end
+
+  defp calculate_sliding_window(_, %{increased_by: x}), do: x
 end
--- test/d1_test.exs
+++ test/d1_test.exs
@@ -1,9 +1,15 @@
 defmodule D1Test do
   @moduledoc false
   use ExUnit.Case
-  import D1, only: [part1: 1]
+  import D1, only: [part1: 1, part2: 1]
 
+  @tag :d1_p1
   test "should produce correct day1 part 1 correct output" do
     assert part1("#{__DIR__}/d1_input.txt") == 7
+  end
+
+  @tag :d1_p2
+  test "should produce correct day1 part 2 correct output" do
+    assert part2("#{__DIR__}/d1_input.txt") == 5
   end
 end
