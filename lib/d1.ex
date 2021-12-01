76cc80c7a79c0f243b173fd3dfded84f57009be5
Angelo Moreira <angelo.moreira@gmail.com>
2021-12-01 23:33:07 +0000

    day 1 part 2 - a much better version based on Brad

--- lib/d1.ex
+++ lib/d1.ex
@@ -2,12 +2,12 @@ defmodule D1 do
   @moduledoc false
   def part1(file) do
     file
-    |> conver_to_ints()
+    |> convert_to_ints()
     |> Enum.reduce(%{increased_by: 0, current_depth: nil}, &calculate_depth(&2, &1))
     |> Map.get(:increased_by)
   end
 
-  defp conver_to_ints(file) do
+  defp convert_to_ints(file) do
     file
     |> File.read!()
     |> String.split("\n")
@@ -26,35 +26,17 @@ defmodule D1 do
 
   def part2(file) do
     file
-    |> conver_to_ints()
-    |> calculate_sliding_window(%{increased_by: 0, window: {}})
+    |> convert_to_ints()
+    |> calculate_sliding_window(0)
   end
 
-  defguard bigger_window(first, second, third, fourth)
-           when second + third + fourth > first + second + third
+  defp calculate_sliding_window([first, _, _, fourth | _] = depths, inc) do
+    inc = if fourth > first, do: inc + 1, else: inc
 
-  defp calculate_sliding_window([new_depth | tail], %{window: {first, second, third}} = acc)
-       when bigger_window(first, second, third, new_depth) do
-    calculate_sliding_window(tail, %{
-      window: {second, third, new_depth},
-      increased_by: Map.get(acc, :increased_by) + 1
-    })
+    depths
+    |> tl()
+    |> calculate_sliding_window(inc)
   end
 
-  defp calculate_sliding_window([new_depth | tail], %{window: {_, second, third}} = acc) do
-    calculate_sliding_window(
-      tail,
-      Map.put(acc, :window, {second, third, new_depth})
-    )
-  end
-
-  # adding tuples to the window until we reach 3
-  defp calculate_sliding_window([head | tail], acc) do
-    new_window = Tuple.append(acc.window, head)
-    acc = Map.put(acc, :window, new_window)
-
-    calculate_sliding_window(tail, acc)
-  end
-
-  defp calculate_sliding_window(_, %{increased_by: x}), do: x
+  defp calculate_sliding_window(_, inc), do: inc
 end
