58d150004e38cc89ee400e25191e7bce7f7eaa16
Angelo Moreira <angelo.moreira@gmail.com>
2021-12-06 20:10:17 +0000

    day 2 part 2

--- lib/d2.ex
+++ lib/d2.ex
@@ -23,4 +23,31 @@ defmodule D2 do
   defp calculate_position({:forward, value}, {x, y}), do: {x + value, y}
   defp calculate_position({:down, value}, {x, y}), do: {x, y + value}
   defp calculate_position({:up, value}, {x, y}), do: {x, y - value}
+
+  def part2(file) do
+    %{depth: d, horizontal: h} =
+      file
+      |> File.read!()
+      |> String.split("\n")
+      |> Enum.map(&turn_to_tuple/1)
+      |> Enum.reduce(%{horizontal: 0, depth: 0, aim: 0}, fn tuple, acc ->
+        calculate_position_with_aim(tuple, acc)
+      end)
+
+    d * h
+  end
+
+  defp calculate_position_with_aim({:forward, value}, %{horizontal: h, depth: d, aim: a} = acc) do
+    acc
+    |> Map.put(:horizontal, h + value)
+    |> Map.put(:depth, d + a * value)
+  end
+
+  defp calculate_position_with_aim({:down, value}, %{aim: a} = acc) do
+    Map.put(acc, :aim, a + value)
+  end
+
+  defp calculate_position_with_aim({:up, value}, %{aim: a} = acc) do
+    Map.put(acc, :aim, a - value)
+  end
 end
--- test/d2_test.exs
+++ test/d2_test.exs
@@ -1,15 +1,15 @@
 defmodule D2Test do
   @moduledoc false
   use ExUnit.Case
-  import D2, only: [part1: 1]
+  import D2, only: [part1: 1, part2: 1]
 
   @tag :d2_p1
   test "should produce correct day2 part 1 correct output" do
     assert part1("#{__DIR__}/d2_input.txt") == 150
   end
 
-  # @tag :d1_p2
-  # test "should produce correct day1 part 2 correct output" do
-  #   assert part2("#{__DIR__}/d1_input.txt") == 5
-  # end
+  @tag :d2_p2
+  test "should produce correct day1 part 2 correct output" do
+    assert part2("#{__DIR__}/d2_input.txt") == 900
+  end
 end
